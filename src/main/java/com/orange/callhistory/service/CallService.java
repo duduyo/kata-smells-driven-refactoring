d75f1f0eea0f6ebe1c1342004e036cab104d9081
Lionel Durand <lionel2.durand@orange.com>
2018-12-09 22:09:02 +0100

    4-refacto CallService first part

--- src/main/java/com/orange/callhistory/service/CallService.java
+++ src/main/java/com/orange/callhistory/service/CallService.java
@@ -1,13 +1,9 @@
 package com.orange.callhistory.service;
 
-import static java.util.stream.Collectors.toList;
-
-import java.util.List;
 import java.util.Optional;
 
 import com.orange.callhistory.controller.CallException;
 import com.orange.callhistory.dao.CallDao;
-import com.orange.callhistory.dao.CallEventDao;
 import com.orange.callhistory.dao.CallRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,56 +14,23 @@ public class CallService {
 
     private CallRepository callRepository;
 
+    private CallDaoMapper callDaoMapper;
+
     @Autowired
-    public CallService(CallRepository callRepository) {
+    public CallService(CallRepository callRepository, CallDaoMapper callDaoMapper) {
         this.callRepository = callRepository;
+        this.callDaoMapper = callDaoMapper;
     }
 
     public void save(Call call) {
-        CallDao callDao = mapCallToDao(call);
+        CallDao callDao = callDaoMapper.mapCallToDao(call);
         callRepository.save(callDao);
-    }
-
-    private CallDao mapCallToDao(Call call) {
-        return new CallDao(
-                call.getCallId(),
-                call.getParticipantTelNumber(),
-                call.getParticipantAnnouncement(),
-                call.getParticipantRingingTimeout(),
-                mapCallEventsToDaos(call));
-    }
-
-    private List<CallEventDao> mapCallEventsToDaos(Call call) {
-        return call.getEvents().stream().map(this::mapCallEventToDao).collect(toList());
-    }
-
-    private CallEventDao mapCallEventToDao(CallEvent callEvent) {
-        return new CallEventDao(callEvent.getId(), callEvent.getStatus(), callEvent.getTimestamp());
     }
 
     public Optional<Call> findCall(String callId) {
         Optional<CallDao> callDao = callRepository.findById(callId);
-        Optional<Call> call = callDao.map(this::mapDaoToCall);
+        Optional<Call> call = callDao.map(callDaoMapper::mapDaoToCall);
         return call;
-    }
-
-    private Call mapDaoToCall(CallDao callDao) {
-        return new Call(
-                callDao.getCallId(),
-                callDao.getParticipantUri(),
-                callDao.getParticipantAnnouncement(),
-                callDao.getParticipantRingingTimeout(),
-                mapDaoToCallEvents(callDao),
-                callDao.getConnectionDate(),
-                callDao.getTerminationDate());
-    }
-
-    private List<CallEvent> mapDaoToCallEvents(CallDao callDao) {
-        return callDao.getEvents().stream().map(this::mapDaoToEvent).collect(toList());
-    }
-
-    private CallEvent mapDaoToEvent(CallEventDao callEventDao) {
-        return new CallEvent(callEventDao.getId(), callEventDao.getStatus(), callEventDao.getTimestamp());
     }
 
     public void createCallIfNotExisting(String callId, Call call) {
@@ -90,4 +53,5 @@ public class CallService {
             throw new CallException("Cannot add event to call with callId " + callId + " : it does not exist");
         }
     }
+
 }
